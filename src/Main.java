951b3baf335c5c75f682af14fec9da082ea033a0
NLBeaz <29271137+AlphaUki@users.noreply.github.com>
2024-09-25 17:20:52 +0200

    Boucle principale ajoutée

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,22 @@ import composants.entities.Entity;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
+	
+	private static List<Balise> balises;
+	
+	private static NiRectangle sky;
+	private static NiRectangle ocean;
+	
 	public static void main(String[] args) {
-		NiLabel label;
+		balises = new ArrayList<Balise>();
+
 		NiSpace space = new NiSpace("Space", new Dimension(1000, 700));
 
 		NiRectangle container = new NiRectangle();
@@ -22,29 +33,41 @@ public class Main {
 		container.setSize(new Dimension(900, 600));
 		container.setLocation(50, 50);
 
-		//Ciel
-		NiRectangle sky = new NiRectangle();
+		// Ciel.
+		sky = new NiRectangle();
 		sky.setBackground(Color.white);
 		sky.setLocation(new Point(0, 0));
 		sky.setSize(new Dimension(900, 300));
 		container.add(sky);
 
-		//Mer
-		NiRectangle ocean = new NiRectangle();
+		// Mer.
+		ocean = new NiRectangle();
 		ocean.setBackground(Color.blue);
 		ocean.setLocation(new Point(0, 300));
 		ocean.setSize(new Dimension(900, 300));
 		container.add(ocean);
 
+		// Balises.
+		addBalises();
 
 		space.add(container);
 		space.openInWindow();
-
-
-		new Balise(ocean, new Point(10, 10));
-		
-
 		space.repaint();
-
+		
+		// Boucle infinie.
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Main.balises.forEach((balise) -> balise.move());
+				System.out.println("test");
+			}
+		}, 0l, 250l);
+	}
+	
+	private static void addBalises() {
+		for (int i = 0; i < 5; i++) {
+			Balise b = new Balise(ocean, new Point(10, 10));
+			balises.add(b);
+		}
 	}
 }
